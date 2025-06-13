9a4fe653cf99abdb25d20e6d89285174001cafb5
0Aakrisht <paakrisht123@gmail.com>
2025-06-13 19:09:00 +0545

    just solved second one also

--- Mastering-Variables/second.js
+++ Mastering-Variables/second.js
@@ -1,3 +1,8 @@
-//create variable name (name) and store your name.
+// 1 .create variable name (name) and store your name.
 let name = "Aakrisht Pachhai" ;
 console.log(name);
+
+// 2 . Create variable name
+// (whatDoYouWannaBecomeInYourLife) & store "programmer"
+let whatDoYouWannaBecomeInYourLife = "Programmer" ;
+console.log(whatDoYouWannaBecomeInYourLife);
