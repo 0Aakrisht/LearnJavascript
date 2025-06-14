79e1f00660f55bff482fcbd03f44347363ce33bd
0Aakrisht <paakrisht123@gmail.com>
2025-06-14 23:21:08 +0545

    solved the second one also

--- Working-with-numbers/main.js
+++ Working-with-numbers/main.js
@@ -1,8 +1,12 @@
 // 1. create variable name (firstFavNum) & store your favorite number.
 let firstFavNum ;
-firstFavNum : 7 ;
+firstFavNum = 7 ;
 console.log(firstFavNum);
-// 2. create variable name (secondFavNum) & store your second fav number.
+// 2. create variable name (secondFavNum) & store your second fav number.\
+let secondFavNum ;
+secondFavNum = 10 ;
+console.log(secondFavNum);
+
 //3. add (firstFavNum and secondFavNum) & store your favorite number .
 //4 . subtract (firstFavNum and secondFavNum) & store your favorite number .
 // 5. Multiply (firstFavNum and secondFavNum) & store your favorite number .
