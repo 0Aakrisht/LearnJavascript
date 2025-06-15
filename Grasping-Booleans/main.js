66a258e120150e08640aa5a8c7a99874749d8bdc
0Aakrisht <paakrisht123@gmail.com>
2025-06-15 21:48:40 +0545

    first coding question of today and it is already almosst 10

---
+++ Grasping-Booleans/main.js
@@ -0,0 +1,13 @@
+//1. create a variable name (isJsProgrammingLanguage) & store true as a value.
+let isJsProgrammingLanguage ;
+isJsProgrammingLanguage = true ;
+console.log(isJsProgrammingLanguage);
+//2. create a variable name (isJsHard) & store false as a value.
+let isJsHard ;
+isJsHard = false ;
+console.log(isJsHard);
+//3. create a variable name (favNumb) & store your favourite number inside.
+let favNumb ;
+favNumb = 7 ;
+//4. Now add the favNumb with the valuer of undefined.
+console.log(favNumb + undefined);
