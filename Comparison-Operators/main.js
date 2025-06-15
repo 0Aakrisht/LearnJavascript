62a905b83e1bc0eb5730f80f2917fa14eca4db59
0Aakrisht <paakrisht123@gmail.com>
2025-06-15 22:32:13 +0545

    second commit of the day

---
+++ Comparison-Operators/main.js
@@ -0,0 +1,18 @@
+//1. create a variable name (firstFavNum) and store your favourite number.
+let firstFavNum = 7 ;
+console.log(firstFavNum);
+//2. create a variable name (secondFavNum) and store your favourite number.
+let secondFavNum = 10 ;
+console.log(secondFavNum);
+//3. check  (firstFavNum is greater than secondFavNum)
+console.log(firstFavNum > secondFavNum);
+//4. check  (firstFavNum is less than secondFavNum)
+console.log(firstFavNum < secondFavNum);
+//5. check  (firstFavNum is greater than and equal to secondFavNum)
+console.log(firstFavNum >= secondFavNum);
+//6. check  (firstFavNum is less than and equal to secondFavNum)
+console.log(firstFavNum <= secondFavNum);
+//7. check  (firstFavNum is equal to secondFavNum) using strict non-equality operator
+//8. check  (firstFavNum is equal to secondFavNum) using loose non-equality operator
+//9. check  (firstFavNum is not equal to secondFavNum) using strict non-equality operator
+//10. check  (firstFavNum is not  equal to secondFavNum) using loose non-equality operator
