8bb05d44da0a021ef24452142b770b886950bb16
0Aakrisht <paakrisht123@gmail.com>
2025-08-04 18:39:06 +0545

    ArrayIntroduction finished

---
+++ 13.ArrayIntroduction/main.js
@@ -0,0 +1,17 @@
+//1.Create array name (favSinger) Store 3 favourite singers.
+let favSinger = ['Durgesh','LilJhola','Jyoti'];
+//2.log the first singer in that array.
+console.log(favSinger[0]);
+//3.create array name (favNumber) and store 4 fav Numbers.
+let favNumber = [7 ,11,10,20];
+//4.create array name (mixedArr) store ["string",["otherarray"],123,true]
+let mixedArr = ['string',['otherarray'],123,true];
+//5. Now Access each item in that array by using [] notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+
+
+
+
