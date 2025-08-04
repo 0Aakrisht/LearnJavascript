2039dfc44d839d20749541c33484f196330315b7
0Aakrisht <paakrisht123@gmail.com>
2025-08-04 12:44:13 +0545

    finished learning it

--- 7.Conditional-Statements/main.js
+++ 7.Conditional-Statements/main.js
@@ -1,2 +1,19 @@
 //1.Create a variable name (password) and assign nothing to it.
 let password ;
+//2.if the password is equal to 8 print "welcome"
+if(password==8){
+    console.log("welcome");
+}
+else if(password<=8)
+{
+    console.log("password is too short");
+}
+else if(password>=8)
+{
+    console.log("password is too long");
+}
+else
+{
+    console.log("please enter a password");
+    
+}
