41f8983dc43dc679aae261d0c66037f11589fbbd
0Aakrisht <paakrisht123@gmail.com>
2025-08-05 00:19:14 +0545

    finished function basics

---
+++ 16.FunctionBasics/main.js
@@ -0,0 +1,11 @@
+//1. Create function name (myFunction)
+//2. This function will take two parameters
+//3. Any number we pass as an argument it will multiply that number.
+//4. As argument pass your favourite number.
+//5. log the number
+function myFunction(num1, num2)
+{
+    return num1*num2;
+}
+let result = myFunction(5,10);
+console.log(result);
