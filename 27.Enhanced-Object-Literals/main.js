40fd46a50699ff704ed21709a80a932d67b006db
0Aakrisht <paakrisht123@gmail.com>
2025-08-31 11:17:57 +0545

    first question of es6 completed

--- 27.Enhanced-Object-Literals/main.js
+++ 27.Enhanced-Object-Literals/main.js
@@ -1,4 +1,27 @@
-setTimeout(() => {
-    console.log("hello world");
+//refractor es5 code to es6 (enhanced object literals)
+//ES5
+//var a = 1 ; 
+// b = 2 ;
+// c = 3 ;
+// obj = {
+// a : a,
+// b : b , 
+//c : c ,
+// };
+ function value(a , b , c) {
+    return {
+        a ,
+        b ,
+        c
+    }
+    again : () =>{
+        console.log(`the value of a is ${a} , b is ${b} and c is ${c}`);   
+    }
     
-}, 1000);
+ } ;
+  
+ const num = value(1 , 2 , 3);
+ console.log(num);
+ console.log(num.again());
+ 
+ 
