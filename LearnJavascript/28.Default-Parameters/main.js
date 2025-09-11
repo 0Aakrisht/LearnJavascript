54f7e955a35d1b39c4baeb86b08a1881e11ffc71
0Aakrisht <paakrisht123@gmail.com>
2025-09-11 18:28:23 +0545

    starting again

---
+++ LearnJavascript/28.Default-Parameters/main.js
@@ -0,0 +1,7 @@
+//1. create function name (multiply) with 2 parameters (a,b) which will multiply the parameters values and return the result.
+//2. provide 1 as the default value to parameter "b".
+function multiply(a , b = 1) {
+    console.log(a*b);
+}
+multiply(10);
+//then the output will be 10 
