60e52038987565455823e22701316ba42cd57c7f
0Aakrisht <paakrisht123@gmail.com>
2025-11-01 14:44:50 +0545

    starting again

---
+++ LearnJavascript/31.Array-Destructing/main.js
@@ -0,0 +1,9 @@
+const colors = ["red", "green", "blue", "yellow", "purple"];
+ 
+//your task is to use array destructuring to extract the first three colors from the colors array and assign them to seperate variables named color1, color2 and color3 respectively.
+const [color1,color2,color3] = colors;
+//after extracting the colors, log them to the console.
+console.log(color1);
+console.log(color2);
+console.log(color3);
+
